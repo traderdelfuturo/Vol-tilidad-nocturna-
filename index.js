2b4d49c7cbcb3694c8fc907e8c28d58c14f00396
traderdelfuturo <issinvestmentindex@gmail.com>
2025-07-24 09:48:57 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ require("./volatilidadNocturna.js");
 require("./volatilidadPreEuropa.js");
 require("./volatilidadLondres.js");
 require("./volatilidadComplementoNY.js");
+require("./auto_vela_m1.js");
