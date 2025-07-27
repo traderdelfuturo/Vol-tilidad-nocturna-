3073b892e299323851559dae792f3f8a0c410c43
traderdelfuturo <issinvestmentindex@gmail.com>
2025-07-27 16:39:17 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ require("./volatilidadPreEuropa.js");
 require("./volatilidadLondres.js");
 require("./volatilidadComplementoNY.js");
 require("./auto_vela_m1.js");
+
+const { startRollup } = require("./rollup_tf.js");
+startRollup();
+
+process.on("unhandledRejection", err => console.error("❌ Unhandled:", err));
+console.log("✅ Servicio iniciado (auto M1 + rollup TF)");
