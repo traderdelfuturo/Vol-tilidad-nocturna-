252466743286262a695378929692356250f9fb66
traderdelfuturo <issinvestmentindex@gmail.com>
2025-12-12 10:25:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ require("./volatilidadPreEuropa.js");
 require("./volatilidadLondres.js");
 require("./volatilidadComplementoNY.js");
 require("./auto_vela_m1.js");
+require("./auto_vela_5s.js"); 
 
 const { startRollup } = require("./rollup_tf.js");
 startRollup();
 
 process.on("unhandledRejection", err => console.error("❌ Unhandled:", err));
-console.log("✅ Servicio iniciado (auto M1 + rollup TF)");
+console.log("✅ Servicio iniciado (auto M1 + auto 5s + rollup TF)");
