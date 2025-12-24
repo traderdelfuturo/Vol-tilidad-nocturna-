4f20576015e58899a6872072d2ccb9a833488376
traderdelfuturo <issinvestmentindex@gmail.com>
2025-12-24 10:59:29 -0500

    Update firebaseApp.js

--- firebaseApp.js
+++ firebaseApp.js
@@ -6,7 +6,7 @@ if (!admin.apps.length) {
   const serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT_JSON);
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://datos-de-mercado-ofa-level-3-default-rtdb.firebaseio.com"
+    databaseURL: "https://datos-de-jdp-default-rtdb.firebaseio.com"
   });
 }
 
