588c2e5f7e87c3ec8a6b410555415069e6fa6107
traderdelfuturo <issinvestmentindex@gmail.com>
2025-12-28 17:24:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require("./volatilidadNocturna.js");
 require("./volatilidadPreEuropa.js");
 require("./volatilidadLondres.js");
 require("./volatilidadComplementoNY.js");
+require("./volatilidadCierre.js"); // <--- NUEVA LÍNEA AÑADIDA
 require("./auto_vela_m1.js");
 require("./auto_vela_5s.js"); 
 
 // Borré las líneas de startRollup porque ya eliminaste el archivo.
 
 process.on("unhandledRejection", err => console.error("❌ Unhandled:", err));
-console.log("✅ Servicio iniciado (auto M1 + auto 5s)");
+console.log("✅ Servicio iniciado (auto M1 + auto 5s + Cierre)");
